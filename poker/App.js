ea58163e471e5f6512b024921d7287b654991e56
JD Grillo <jdgrillo@JDs-MacBook-Air.local>
2020-02-15 13:01:47 -0600

    card randomization

--- poker/App.js
+++ poker/App.js
@@ -1,8 +1,17 @@
 // Render the cards using React!
 const App = (props) => {
     let { cards } = props;
+    let card1Path = `cards/${cards[0].value}${cards[0].suit}.png`
+    let card2Path = `cards/${cards[1].value}${cards[1].suit}.png`
+    let cardStyle = {
+        width: 150,
+        height: 300
+    }
     return (
-        <span>cards</span>
+        <div>
+            <img src={(card1Path)} style={cardStyle}></img>
+            <img src={(card2Path)} style={cardStyle}></img>
+        </div>
     )
 }
 
