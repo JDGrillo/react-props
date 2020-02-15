d77fde3a6f215596a5af57f9900de2934e458ee1
JD Grillo <jdgrillo@JDs-MacBook-Air.local>
2020-02-15 12:52:28 -0600

    rendering and adding random circles

--- circles/App.js
+++ circles/App.js
@@ -1,9 +1,25 @@
 // Render the circles using React!
 const App = (props) => {
+    // assign props into circles
     let { circles } = props;
+    // create circle array to keep track of all circles with unique styles
+    let circleDivs = []
+    for (let i = 0; i < circles.length; i++){
+        let myStyles = {
+            height: circles[i].radius,
+            width: circles[i].radius,
+            backgroundColor: circles[i].color,
+            borderRadius: 50
+        }
+        // add each circle with unique style into an array
+        // can keep in jsx format
+        circleDivs.push(<div style={myStyles}></div>)
+    }
     return (
-        <span>circles</span>
+        <div>
+            {circleDivs}
+        </div>
     )
 }
 
-const Circle = null;  // CHALLENGE: Write a separate Circle component for use in the App component
+//const Circle = null;  // CHALLENGE: Write a separate Circle component for use in the App component
